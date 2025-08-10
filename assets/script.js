f76c651b56bd179036d47c137bbf2205a29e13fc
KaeriKalmar <kaerigraphix@gmail.com>
2025-08-10 21:57:01 +0200

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -282,7 +282,7 @@ const alreadyActivatedCodes = ["MASTER_ACCESS_2025!@#$","CS110T2_BONUS014 &4", "
     
     //added individually 
     "BI110_TOTAL_ACCESS",
-    "FULL_UNLOCK_2025++","ACCESS_GRANTED_GLOBAL$","BI110_BUNDLE_PACK_123","SUPER_USER_XYZ_789"
+    "FULL_UNLOCK_2025++","ACCESS_GRANTED_GLOBAL$","BI110_BUNDLE_PACK_123","SUPER_USER_XYZ_789","CBU_UNLOCK_CODE🔥"
     
   
     // Example: If "CS110T2_MAIN001#A@" was sold and activated by someone,
